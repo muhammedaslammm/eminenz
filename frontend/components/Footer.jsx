c7b9422a0195eb6b34c9d71736c6d84ffa79f51e
Muhammed Aslam <muhammedbinramli@gmail.com>
2025-06-27 07:08:08 +0530

    hope error rectified

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -7,19 +7,20 @@ import {
   copyright,
   media,
 } from "@/data/footerdata";
-import usePageInfo from "@/utils/usePageInfo";
+import { usePathname } from "next/navigation";
 
 const Footer = () => {
-  const { pagestyle } = usePageInfo();
-  console.log("style for the current page:", pagestyle);
+  const home = usePathname() === "/";
+  const color = home ? "rgba(180,211,227,0.65)" : "rgb(23, 23, 23)";
+  const text_color = home ? "rgb(23, 23, 23)" : "#ffffff";
   return (
     <footer
       className="pt-5 lg:pt-10 pb-1 lg:pb-2"
-      style={{ backgroundColor: pagestyle.bg_color }}
+      style={{ backgroundColor: color }}
     >
       <div
         className="w-[90%] lg:w-[87%] mx-auto "
-        style={{ color: pagestyle.main_text }}
+        style={{ color: text_color }}
       >
         {/* top */}
         <div className="leading-[1.3rem] text-center md:text-left lg:leading-[2.2rem]">
@@ -28,7 +29,7 @@ const Footer = () => {
           </h3>
           <h4
             className="text-[.8rem] lg:text-[1.1rem] tracking-[.05rem] uppercase"
-            style={{ color: pagestyle.sub_text }}
+            style={{ color: text_color }}
           >
             {title.tagline}
           </h4>
@@ -38,7 +39,7 @@ const Footer = () => {
           <div className="flex flex-col w-[40%] lg:w-full lg:flex-row items-center md:items-start gap-2 lg:gap-8 uppercase">
             <div
               className="text-[.9rem] lg:text-[1rem]"
-              style={{ color: pagestyle.sub_text }}
+              style={{ color: text_color }}
             >
               {navcontent.title}
             </div>
@@ -60,10 +61,7 @@ const Footer = () => {
                 key={c.id}
                 className="text-[.9rem] text-center md:text-left lg:text-[1rem]"
               >
-                <span
-                  className="block uppercase"
-                  style={{ color: pagestyle.sub_text }}
-                >
+                <span className="block uppercase" style={{ color: text_color }}>
                   {c.label}
                 </span>
                 <span className="font-medium">{c.value}</span>
@@ -74,7 +72,7 @@ const Footer = () => {
         {/* bottom */}
         <div
           className="flex flex-col md:flex-row md:justify-between items-center text-[.7rem] lg:text-[.8rem]"
-          style={{ color: pagestyle.sub_text }}
+          style={{ color: text_color }}
         >
           <div>{copyright}</div>
           <ul className="flex gap-4">
