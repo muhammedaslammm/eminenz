31ee1084f5f1a3d8010d71ab50dfbac9d33769d0
Muhammed Aslam <muhammedbinramli@gmail.com>
2025-06-29 10:04:07 +0530

    industries gap

--- frontend/components/Industries.client.jsx
+++ frontend/components/Industries.client.jsx
@@ -28,7 +28,7 @@ const Industries = ({ data }) => {
       <p className="text-[1.2rem] font-medium leading-[1.6rem] lg:text-[1.3rem]">
         {data.title}
       </p>
-      <div className="grid grid-cols-1 lg:grid-cols-8 gap-1 lg:gap-5">
+      <div className="grid grid-cols-2 lg:grid-cols-8 gap-3 lg:gap-5">
         {data.icons.map(({ icon, ...rest }) => {
           let Icon = iconMap[icon];
           return (
@@ -37,7 +37,7 @@ const Industries = ({ data }) => {
               className="flex flex-col flex-wrap items-center gap-2 border border-[rgba(58,83,96,0.63)] lg:border-0 lg:bg-[#11416F]/25 text-[#11416F] py-6 lg:py-8 rounded-[.2rem] lg:rounded-[.5rem] transition-all"
             >
               <Icon className="w-8 h-8" weight="light" />
-              <span className="uppercase text-center text-[.8rem] lg:text-[.8rem] lg:leading-[1.2rem] font-semibold px-2 max-w-[6.5rem] break-words">
+              <span className="uppercase text-center text-[.9rem] lg:text-[.8rem] lg:leading-[1.2rem] font-semibold px-2 max-w-[6.5rem] break-words">
                 {rest.title}
               </span>
             </div>
